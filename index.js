73fb10b580aba9b4c71d3e9d75b7cb264105bbd0
gitaumungai672 <gitaumungai50@gmail.com>
2025-12-30 16:53:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Attendance Backend is running");
+});
+
 app.get("/health", (req, res) => {
   res.send("OK");
 });
